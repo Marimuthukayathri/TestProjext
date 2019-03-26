6aa19a9c66397704a88c26c859d5e806bbe0cff1
Mathan <Mathan@Mathan-PC>
2019-03-26 22:04:15 +0530

    Test class added

---
+++ TestProjext/src/main/java/TestProj/TestProjext/Test1.java
@@ -0,0 +1,12 @@
+package TestProj.TestProjext;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		System.out.println("Hai GIT");
+
+	}
+
+}
